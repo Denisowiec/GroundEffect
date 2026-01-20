f634add65aba54e7e5384656807d4af440cc8dfb
Denisowiec <ddspeed@gmail.com>
2026-01-20 20:05:00 +0100

    The app now displays a card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,71 @@
 <script setup>
-import cardRow from './cardrow.vue'
+
+// Program's internal logic
+function shuffle(array) {
+  // Fisher-Yatess shuffle algorith, taken from w3schools JS tutorial
+  for (let i = array.length -1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i+1));
+    let k = array[i];
+    array[i] = array[j];
+    array[j] = k;
+  }
+}
+
+function generateDeck(colors) {
+  // Accepts an array of color names
+  // Returns an array of objects containing the color name, a corner speed value and a straight speed value
+  const possibleCards = [
+    [0, 10],
+    [0, 11],
+    [1, 12],
+    [1, 13],
+    [1, 14],
+    [2, 15],
+    [2, 16],
+    [2, 17],
+    [3, 18],
+    [3, 19]
+  ]
+
+  const numberOfColors = colors.length
+  const deck = []
+
+  // This prepares a shuffled set of 10 options that don't repeat, for every color
+  const numbersByColor = new Map()
+  for (const col of colors) {
+    numbersByColor.set(col, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+  }
+  for (const c of numbersByColor.values()) {
+    shuffle(c)
+  }
+
+  // We now create cards for the deck, by selecting possible cards one by one for each color
+  for (let i = 0; i < 10; i++) {
+    let card = []
+    for (const col of colors) {
+      let speedValues = possibleCards[numbersByColor.get(col)[i]]
+      card.push({ color: col, cornerSpeed: speedValues[0], straightSpeed: speedValues[1]})
+    }
+    deck.push(card)
+  }
+  // TODO: For the time being, the algorith allows for the same values for two different
+  // colors on the same cards. This shouldn't happen, assuming there are fewer than 10 colors.
+  return deck
+}
+
+function nextCard() {
+  console.log("nextCard executed")
+  console.log(current)
+  current++
+  if (current >= 10) {
+    current = 0
+    deck = generateDeck(colors)
+  }
+}
+const colors = ["black", "blue", "green", "grey", "orange", "purple", "red", "yellow", "purple"]
+const deck = generateDeck(["black", "blue", "green", "grey", "orange", "purple", "red", "yellow", "purple"])
+let current = 0
+
 </script>
 
 <template>
@@ -15,8 +81,13 @@ import cardRow from './cardrow.vue'
         </tr>
       </thead>
       <tbody>
-        <cardRow color="blue" corner-speed="0" straight-speed="10"/>
+        <tr v-for="row in deck[current]">
+          <td><img :src="'./assets/cars/' + row.color + '_car.png'"></td>
+          <td>{{ row.cornerSpeed }}</td>
+          <td>{{ row.straightSpeed }}</td>
+        </tr>
       </tbody>
+      <div class="card_footer"><span id="card_counter">{{ current + " / 10" }}</span><button id="button_next" @:click="nextCard()">Next card</button></div>
     </table>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,60 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Program's internal logic
+function shuffle(array) {
+    // Fisher-Yatess shuffle algorith, taken from w3schools JS tutorial
+    for (let i = array.length -1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i+1));
+        let k = array[i];
+        array[i] = array[j];
+        array[j] = k;
+    }
+}
+
+function generateDeck(colors) {
+    // Accepts an array of color names
+    // Returns an array of objects containing the color name, a corner speed value and a straight speed value
+    const possibleCards = [
+        [0, 10],
+        [0, 11],
+        [1, 12],
+        [1, 13],
+        [1, 14],
+        [2, 15],
+        [2, 16],
+        [2, 17],
+        [3, 18],
+        [3, 19]
+    ]
+
+    const numberOfColors = colors.length
+    const deck = []
+
+    // This prepares a shuffled set of 10 options that don't repeat, for every color
+    const numbersByColor = new Map()
+    for (const col of colors) {
+        numbersByColor.set(col, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    }
+    for (const c of numbersByColor.values()) {
+        shuffle(c)
+    }
+
+    // We now create cards for the deck, by selecting possible cards one by one for each color
+    for (let i = 0; i < 10; i++) {
+        let card = []
+        for (const col of colors) {
+            let speedValues = possibleCards[numbersByColor.get(col)[i]]
+            card.push({ color: col, cornerSpeed: speedValues[0], straightSpeed: speedValues[1]})
+        }
+        deck.push(card)
+    }
+    // TODO: For the time being, the algorith allows for the same values for two different
+    // colors on the same cards. This shouldn't happen, assuming there are fewer than 10 colors.
+    return deck
+}
+
+const deck = generateDeck(["black", "blue", "green", "grey", "orange", "purple", "red", "yellow", "purple"])
+let current = 0
+
 createApp(App).mount('#app')
